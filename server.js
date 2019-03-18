eba929ae61f1718f384ca04c3cbf1319ccf10a2a
shannon <shannonrmoranetz@gmail.com>
2019-03-18 16:03:21 -0600

    Add initial endpoint methods to server

---
+++ server.js
@@ -0,0 +1,29 @@
+import '@babel/polyfill';
+import express from 'express';
+const environment = process.env.NODE_ENV || 'development';
+const app = express();
+app.use(express.json());
+app.set('port', process.env.PORT || 3001);
+
+// GET:
+// All projects
+app.get('/api/v1/projects', async (req, res) => {
+  try {
+    res.status(200).json('Hello Projects');
+  } catch (error) {
+    res.status(500).json({ error });
+  } 
+});
+
+// All palettes
+app.get('/api/v1/palettes', async (req, res) => {
+  try {
+    res.status(200).json('Hello Palettes');
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port: ${app.get('port')}`);
+});
