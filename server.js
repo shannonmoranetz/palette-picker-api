79ac606ac1105613b64ac2219fdfa7e555c715bd
Heather Hartley <hlhartley@gmail.com>
2019-03-19 14:50:11 -0600

    Add get method for one palette

--- server.js
+++ server.js
@@ -19,29 +19,42 @@ app.get('/api/v1/projects', async (req, res) => {
 });
 
 // All palettes for one project
-app.get('api/v1/projects/:id/palettes', async (req, res) => {
+app.get('/api/v1/projects/:id/palettes', async (req, res) => {
     try {
-      const allPalettes = await database('palettes').select();
-      const selectedProject = await database('projects').where('id', parseInt(request.params.id));
-      let palettesToReturn = await allPalettes.filter((palette) => {
-        return selectedProject.id === palette.project_id
-      });
-      res.status(200).json(palettesToReturn);
+      const projectId = req.params.id
+      const palettesForProject = await database('palettes').where('project_id', projectId);
+      // **Query (conditional logic to further narrow down palettesForProject)
+      res.status(200).json({ palettes: palettesForProject });
     } catch (error) {
         res.status(500).json({ error });
     }
 });
 
-// One palette
-app.get('api/v1/palettes/:id', async (req, res) => {
+// One project
+app.get('/api/v1/projects/:id', async (req, res) => {
   try {
-    res.status(200).json('Hello 1 palette');
+    const projectId = req.params.id
+    const selectedProject = await database('projects').where('id', projectId)
+    // if (selectedProject.length) {
+      res.status(200).json({ project: selectedProject });
+    // } else {
+    //   res.status(404).json({ error: `No project found with the id of ${projectId}.` })
+    // }
   } catch (error) {
     res.status(500).json({ error })
   }
 });
 
-// Dynamic query 
+// One palette
+app.get('/api/v1/palettes/:id', async (req, res) => {
+  try {
+    const paletteId = req.params.id
+    const selectedPalette = await database('palettes').where('id', paletteId)
+    res.status(200).json({ palette: selectedPalette });
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Server running on port: ${app.get('port')}`);
