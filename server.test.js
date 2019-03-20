4f580268e3d4dbb3f7c2174a08429e51a0725d90
shannon <shannonrmoranetz@gmail.com>
2019-03-20 17:08:12 -0600

    Add get method endpoint tests

--- server.test.js
+++ server.test.js
@@ -22,11 +22,33 @@ describe('Server', () => {
     });
   });
   describe('GET /api/v1/projects/:id/palettes', () => {
-    it.skip('Should return all palettes in the database a project with matching params', async () => {
+    it.skip('Should return all palettes in the database for a single project', async () => {
       const numExpectedPalettes = palettes.length;
       const response = await request(app).get('/api/v1/projects/1/palettes');
       const result = response.body;
       expect(result.length).toEqual(numExpectedPalettes);
+    });
+  });
+  describe('GET /api/v1/projects/:id', () => {
+    it('Should return a single project', async () => {
+      const expectedProject = await database('projects').first();
+      const id = expectedProject.id;
+      const response = await request(app).get(`/api/v1/projects/${id}`)
+      const result = response.body.project[0]
+      expect(result).toEqual(expectedProject)
+    });
+    it.skip('Should return an error if the project does not exist', async () => {
+    });
+  });
+  describe('GET /api/v1/palettes/:id', () => {
+    it('Should return a single palette', async () => {
+      const expectedPalette = await database('palettes').first();
+      const id = expectedPalette.id;
+      const response = await request(app).get(`/api/v1/palettes/${id}`)
+      const result = response.body.palette[0]
+      expect(result).toEqual(expectedPalette)
+    });
+    it.skip('Should return an error if the palette does not exist', async () => {
     });
   });
 });
