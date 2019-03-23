3df34a4f9e00520fefc6aa0ff029dfd3b2b6e64e
shannon <shannonrmoranetz@gmail.com>
2019-03-23 13:38:04 -0600

    Add delete tests

--- server.js
+++ server.js
@@ -61,29 +61,29 @@ app.get('/api/v1/projects/:id/palettes', async (req, res) => {
 app.get('/api/v1/projects/:id', async (req, res) => {
   try {
     const projectId = req.params.id
-    const selectedProject = await database('projects').where('id', projectId)
+    const selectedProject = await database('projects').where('id', projectId);
     if (selectedProject.length) {
       res.status(200).json({ project: selectedProject });
     } else {
-      res.status(404).json({ error: `No project found with the id of ${projectId}.` })
+      res.status(404).json({ error: `No project found with the id of ${projectId}.` });
     }
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 });
 
 // One palette
 app.get('/api/v1/palettes/:id', async (req, res) => {
   try {
-    const paletteId = req.params.id
-    const selectedPalette = await database('palettes').where('id', paletteId)
+    const paletteId = req.params.id;
+    const selectedPalette = await database('palettes').where('id', paletteId);
     if (selectedPalette.length) {
       res.status(200).json({ palette: selectedPalette });
     } else {
-      res.status(404).json({ error: `No palette found with the id of ${paletteId}.` })
+      res.status(404).json({ error: `No palette found with the id of ${paletteId}.` });
     }
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 });
 
@@ -91,8 +91,8 @@ app.get('/api/v1/palettes/:id', async (req, res) => {
 // One project
 app.post('/api/v1/projects', async (req, res) => {
   try {
-    const project = req.body
-    if (!project.name) return res.status(422).send({ error: `Expected format: { name: <String> }` })
+    const project = req.body;
+    if (!project.name) return res.status(422).send({ error: `Expected format: { name: <String> }` });
     database('projects').insert(project, 'id')
       .then(projectIds => {
         res.status(201).json({ id: projectIds[0] })
@@ -105,7 +105,7 @@ app.post('/api/v1/projects', async (req, res) => {
 // One palette
 app.post('/api/v1/palettes', async (req, res) => {
   try {
-    const { project_id, name, color1, color2, color3, color4, color5 } = req.body
+    const { project_id, name, color1, color2, color3, color4, color5 } = req.body;
     if (!project_id || !name || !color1 || !color2 || !color3 || !color4 || !color5) {
       return res.status(422).send({ 
         error: `Expected format: { project_id: <Integer>, name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String> }` 
@@ -116,7 +116,7 @@ app.post('/api/v1/palettes', async (req, res) => {
         res.status(201).json({ id: paletteIds[0] })
       })
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 });
 
@@ -126,7 +126,7 @@ app.put('/api/v1/projects/:id', (req, res) => {
   try {
     const updates = req.body;
     if (!updates.name) {
-      return res.status(422).send({ 
+      return res.status(422).send({
         error: `Expected format: { name: <String> }`
       });
     };
@@ -135,7 +135,7 @@ app.put('/api/v1/projects/:id', (req, res) => {
         res.status(202).json({ id: projectIds[0] })
       });
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   };
 })
 
@@ -148,10 +148,10 @@ app.put('/api/v1/palettes/:id', async (req, res) => {
       const updatedPalette = await database('palettes').where('id', parseInt(req.params.id)).select().update(updates, 'id');
       res.status(202).json({ id: updatedPalette[0] })
     } else {
-      res.status(404).json({ error: `No palette found with the id of ${req.params.id}.` })
+      res.status(404).json({ error: `No palette found with the id of ${req.params.id}.` });
     }
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 })
 
@@ -159,32 +159,32 @@ app.put('/api/v1/palettes/:id', async (req, res) => {
 // One project
 app.delete('/api/v1/projects/:id', async (req, res) => {
   try {
-    const projectId = req.params.id
-    const projectToDelete = await database('projects').where('id', parseInt(req.params.id)).select()
+    const projectId = req.params.id;
+    const projectToDelete = await database('projects').where('id', parseInt(req.params.id)).select();
     if (projectToDelete.length) {
-      await database('projects').where('id', parseInt(req.params.id)).select().delete()
-      res.status(202).json({ id: projectToDelete[0].id} )
+      await database('projects').where('id', parseInt(req.params.id)).select().delete();
+      res.status(202).json({ id: projectToDelete[0].id} );
     } else {
-      res.status(404).json({ error: `No project found with the id of ${projectId}.` })
+      res.status(404).json({ error: `No project found with the id of ${projectId}.` });
     }
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 })
 
 // One palette
 app.delete('/api/v1/palettes/:id', async (req, res) => {
   try {
-    const paletteId = req.params.id
-    const paletteToDelete = await database('palettes').where('id', parseInt(req.params.id)).select()
+    const paletteId = req.params.id;
+    const paletteToDelete = await database('palettes').where('id', parseInt(req.params.id)).select();
     if (paletteToDelete.length) {
-      await database('palettes').where('id', parseInt(req.params.id)).select().delete()
-      res.status(202).json({ id: paletteToDelete[0].id } )
+      await database('palettes').where('id', parseInt(req.params.id)).select().delete();
+      res.status(202).json({ id: paletteToDelete[0].id });
     } else {
-      res.status(404).json({ error: `No palette found with the id of ${paletteId}.` })
+      res.status(404).json({ error: `No palette found with the id of ${paletteId}.` });
     }
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
 })
 
--- server.test.js
+++ server.test.js
@@ -8,7 +8,7 @@ import projects from './db/data/example-projects';
 describe('Server', () => {
 
   beforeEach(async () => {
-    await database.seed.run()
+    await database.seed.run();
   });
 
   describe('Init', () => {
@@ -16,176 +16,198 @@ describe('Server', () => {
       const response = await request(app).get('/');
       expect(response.status).toEqual(200);
     });
-  });
 
-  describe('GET /api/v1/projects', () => {
-    it('Should return all projects in the database', async () => {
-      const numExpectedProjects = projects.length;
-      const response = await request(app).get('/api/v1/projects');
-      const result = response.body;
-      expect(result.length).toEqual(numExpectedProjects);
+    describe('GET /api/v1/projects', () => {
+      it('Should return all projects in the database', async () => {
+        const numExpectedProjects = projects.length;
+        const response = await request(app).get('/api/v1/projects');
+        const result = response.body;
+        expect(result.length).toEqual(numExpectedProjects);
+      });
     });
-  });
-
-  describe('GET /api/v1/projects/:id', () => {
-    it('Should return a single project', async () => {
-      const expectedProject = await database('projects').first();
-      const id = expectedProject.id;
-      const response = await request(app).get(`/api/v1/projects/${id}`);
-      const result = response.body.project[0].name;
-      expect(result).toEqual(expectedProject.name);
-    });
-    it('Should return an error if the project does not exist', async () => {
-      const projectId = 0;
-      const expectedError = `No palettes found for project with the id of ${projectId}.`;
-      const response = await request(app).get(`/api/v1/projects/${projectId}/palettes`);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
-    });
-  });
-
-  describe('GET /api/v1/palettes/:id', () => {
-    it('Should return a single palette', async () => {
-      const expectedPalette = await database('palettes').first();
-      const id = expectedPalette.id;
-      const response = await request(app).get(`/api/v1/palettes/${id}`);
-      const result = response.body.palette[0].color1;
-      expect(result).toEqual(expectedPalette.color1);
-    });
-    it('Should return an error if the palette does not exist', async () => {
-      const paletteId = 0;
-      const expectedError = `No palette found with the id of ${paletteId}.`;
-      const response = await request(app).get(`/api/v1/palettes/${paletteId}`);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
-    });
-  });
-
-  describe('GET /api/v1/projects/:id/palettes', () => {
-    it('Should return all palettes in the database for a single project', async () => {
-      const firstProject = await database('projects').first();
-      const projectId = firstProject.id;
-      const numExpectedPalettes = projects.find((project) => {
-        return project.name === firstProject.name
-      }).palettes.length;
-      const response = await request(app).get(`/api/v1/projects/${projectId}/palettes`);
-      const result = response.body.palettes;
-      expect(result.length).toEqual(numExpectedPalettes);
-    });
-    it('Should return all palettes in the database for a project that matches the hexcode query', async () => {
-      const matchingPalette = await database('palettes').where('color1', 'a12345');
-      const firstProject = await database('projects').first();
-      const projectId = firstProject.id;
-      const numExpectedPalettesByHex = matchingPalette.length;
-      const response = await request(app).get(`/api/v1/projects/${projectId}/palettes?hex=a12345`);
-      const result = response.body.palettes;
-      expect(result.length).toEqual(numExpectedPalettesByHex);
-    });
-    it('Should return an error if the hexcode query does not match any palettes', async () => {
-      const hexcodeQuery = 'z0z19z';
-      const expectedError = `No palettes found for hex code with the value of ${hexcodeQuery}.`;
-      const response = await request(app).get(`/api/v1/projects/1/palettes?hex=${hexcodeQuery}`);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
-    });
-  });
-
-  describe('POST /api/v1/projects', () => {
-    it('Should post a new project to the database', async () => {
-      const newProject = { name: 'newMockName' };
-      const response = await request(app).post('/api/v1/projects').send(newProject);
-      const results = await database('projects').where('id', parseInt(response.body.id));
-      const project = results[0];
-      expect(project.name).toEqual(newProject.name);
-    });
-    it('Should return an error if a project does not have a name', async () => {
-      const newProject = {};
-      const expectedError = `Expected format: { name: <String> }`;
-      const response = await request(app).post(`/api/v1/projects`).send(newProject);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
-    });
-  });
   
-  describe('POST /api/v1/palettes', () => {
-    it('Should post a new palette to the database', async () => {
-      const firstPalette = await database('palettes').first();
-      const paletteId = firstPalette.project_id;
-
-      const newPalette = { project_id: paletteId, name: 'newMockName', color1: 'aaaaaa', color2: 'bbbbbb', color3: 'cccccc', color4: 'dddddd', color5: 'eeeeee' };
-      const response = await request(app).post('/api/v1/palettes').send(newPalette);
-      const results = await database('palettes').where('id', parseInt(response.body.id));
-      const palette = results[0];
-      expect(palette.name).toEqual(newPalette.name);
+    describe('GET /api/v1/projects/:id', () => {
+      it('Should return a single project', async () => {
+        const expectedProject = await database('projects').first();
+        const id = expectedProject.id;
+        const response = await request(app).get(`/api/v1/projects/${id}`);
+        const result = response.body.project[0].name;
+        expect(result).toEqual(expectedProject.name);
+      });
+  
+      it('Should return an error if the project does not exist', async () => {
+        const projectId = 0;
+        const expectedError = `No palettes found for project with the id of ${projectId}.`;
+        const response = await request(app).get(`/api/v1/projects/${projectId}/palettes`);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-    it('Should return an error if a palette does not have the required values', async () => {
-      const newPalette = {};
-      const expectedError = `Expected format: { project_id: <Integer>, name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String> }`;
-      const response = await request(app).post(`/api/v1/palettes`).send(newPalette);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
+  
+    describe('GET /api/v1/palettes/:id', () => {
+      it('Should return a single palette', async () => {
+        const expectedPalette = await database('palettes').first();
+        const id = expectedPalette.id;
+        const response = await request(app).get(`/api/v1/palettes/${id}`);
+        const result = response.body.palette[0].color1;
+        expect(result).toEqual(expectedPalette.color1);
+      });
+  
+      it('Should return an error if the palette does not exist', async () => {
+        const paletteId = 0;
+        const expectedError = `No palette found with the id of ${paletteId}.`;
+        const response = await request(app).get(`/api/v1/palettes/${paletteId}`);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-  });
-
-  describe('PUT /api/v1/projects/:id', () => {
-    it('Should update an existing project in the database', async () => {
-      const projectToUpdate = await database('projects').first();
-      const projectId = projectToUpdate.id;
-      const updatesToMake = { name: 'newChangedProject' };
-      await request(app).put(`/api/v1/projects/${projectId}`).send(updatesToMake);
-      const result = await database('projects').where('id', projectId);
-      const project = result[0];
-      expect(project.name).toEqual(updatesToMake.name)
+  
+    describe('GET /api/v1/projects/:id/palettes', () => {
+      it('Should return all palettes in the database for a single project', async () => {
+        const firstProject = await database('projects').first();
+        const projectId = firstProject.id;
+        const numExpectedPalettes = projects.find((project) => {
+          return project.name === firstProject.name
+        }).palettes.length;
+        const response = await request(app).get(`/api/v1/projects/${projectId}/palettes`);
+        const result = response.body.palettes;
+        expect(result.length).toEqual(numExpectedPalettes);
+      });
+  
+      it('Should return all palettes in the database for a project that matches the hexcode query', async () => {
+        const matchingPalette = await database('palettes').where('color1', 'a12345');
+        const firstProject = await database('projects').first();
+        const projectId = firstProject.id;
+        const numExpectedPalettesByHex = matchingPalette.length;
+        const response = await request(app).get(`/api/v1/projects/${projectId}/palettes?hex=a12345`);
+        const result = response.body.palettes;
+        expect(result.length).toEqual(numExpectedPalettesByHex);
+      });
+  
+      it('Should return an error if the hexcode query does not match any palettes', async () => {
+        const hexcodeQuery = 'z0z19z';
+        const expectedError = `No palettes found for hex code with the value of ${hexcodeQuery}.`;
+        const response = await request(app).get(`/api/v1/projects/1/palettes?hex=${hexcodeQuery}`);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-    it('Should return an error if a project does not have the required name to update', async () => {
-      const projectToUpdate = await database('projects').first();
-      const projectId = projectToUpdate.id;
-      const updatesToMake = {};
-      const expectedError = `Expected format: { name: <String> }`;
-      const response = await request(app).put(`/api/v1/projects/${projectId}`).send(updatesToMake);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
+  
+    describe('POST /api/v1/projects', () => {
+      it('Should post a new project to the database', async () => {
+        const newProject = { name: 'newMockName' };
+        const response = await request(app).post('/api/v1/projects').send(newProject);
+        const results = await database('projects').where('id', parseInt(response.body.id));
+        const project = results[0];
+        expect(project.name).toEqual(newProject.name);
+      });
+  
+      it('Should return an error if a project does not have a name', async () => {
+        const newProject = {};
+        const expectedError = `Expected format: { name: <String> }`;
+        const response = await request(app).post(`/api/v1/projects`).send(newProject);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-  });
-
-  describe('PUT /api/v1/palettes/:id', () => {
-    it('Should update an existing palette in the database', async () => {
-      const paletteToUpdate = await database('palettes').first();
-      const paletteId = paletteToUpdate.id;
-      const updatesToMake = { color1: 'zzzzzz' };
-      await request(app).put(`/api/v1/palettes/${paletteId}`).send(updatesToMake);
-      const result = await database('palettes').where('id', paletteId);
-      const palette = result[0];
-      expect(palette.color1).toEqual(updatesToMake.color1)
+    
+    describe('POST /api/v1/palettes', () => {
+      it('Should post a new palette to the database', async () => {
+        const firstPalette = await database('palettes').first();
+        const paletteId = firstPalette.project_id;
+        const newPalette = { project_id: paletteId, name: 'newMockName', color1: 'aaaaaa', color2: 'bbbbbb', color3: 'cccccc', color4: 'dddddd', color5: 'eeeeee' };
+        const response = await request(app).post('/api/v1/palettes').send(newPalette);
+        const results = await database('palettes').where('id', parseInt(response.body.id));
+        const palette = results[0];
+        expect(palette.name).toEqual(newPalette.name);
+      });
+  
+      it('Should return an error if a palette does not have the required values', async () => {
+        const newPalette = {};
+        const expectedError = `Expected format: { project_id: <Integer>, name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String> }`;
+        const response = await request(app).post(`/api/v1/palettes`).send(newPalette);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-
-    it('Should return an error with a PUT to non-existent ID', async () => {
-      const paletteId = 0;
-      const updatesToMake = { color1: 'zzzzzz' };
-      const expectedError = `No palette found with the id of ${paletteId}.`;
-      const response = await request(app).put(`/api/v1/palettes/${paletteId}`).send(updatesToMake);
-      const result = response.body.error;
-      expect(expectedError).toEqual(result);
+  
+    describe('PUT /api/v1/projects/:id', () => {
+      it('Should update an existing project in the database', async () => {
+        const projectToUpdate = await database('projects').first();
+        const projectId = projectToUpdate.id;
+        const updatesToMake = { name: 'newChangedProject' };
+        await request(app).put(`/api/v1/projects/${projectId}`).send(updatesToMake);
+        const result = await database('projects').where('id', projectId);
+        const project = result[0];
+        expect(project.name).toEqual(updatesToMake.name);
+      });
+  
+      it('Should return an error if a project does not have the required name to update', async () => {
+        const projectToUpdate = await database('projects').first();
+        const projectId = projectToUpdate.id;
+        const updatesToMake = {};
+        const expectedError = `Expected format: { name: <String> }`;
+        const response = await request(app).put(`/api/v1/projects/${projectId}`).send(updatesToMake);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-  });
-
-  describe('DELETE /api/v1/projects/:id', () => {
-    it('Should delete an existing project in the database', async () => {
-
+  
+    describe('PUT /api/v1/palettes/:id', () => {
+      it('Should update an existing palette in the database', async () => {
+        const paletteToUpdate = await database('palettes').first();
+        const paletteId = paletteToUpdate.id;
+        const updatesToMake = { color1: 'zzzzzz' };
+        await request(app).put(`/api/v1/palettes/${paletteId}`).send(updatesToMake);
+        const result = await database('palettes').where('id', paletteId);
+        const palette = result[0];
+        expect(palette.color1).toEqual(updatesToMake.color1);
+      });
+  
+      it('Should return an error with a PUT to non-existent ID', async () => {
+        const paletteId = 0;
+        const updatesToMake = { color1: 'zzzzzz' };
+        const expectedError = `No palette found with the id of ${paletteId}.`;
+        const response = await request(app).put(`/api/v1/palettes/${paletteId}`).send(updatesToMake);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-
-    it.skip('Should return an error with a DELETE to non-existent ID', async () => {
-
+  
+    describe('DELETE /api/v1/projects/:id', () => {
+      it('Should delete an existing project in the database', async () => {
+        const projectToDelete = await database('projects').where('name', 'Neutral colors project');
+        const projectId = projectToDelete[0].id;
+        await request(app).delete(`/api/v1/projects/${projectId}`);
+        const deletedProject = await database('projects').where('id', projectId);
+        expect(deletedProject.length).toEqual(0);
+      });
+  
+      it('Should return an error with a DELETE to non-existent ID', async () => {
+        const projectId = 0;
+        const expectedError = `No project found with the id of ${projectId}.`;
+        const response = await request(app).delete(`/api/v1/projects/${projectId}`);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
-  });
-
-  describe('DELETE /api/v1/palettes/:id', () => {
-    it.skip('Should delete an existing palette in the database', async () => {
-
-    });
-
-    it.skip('Should return an error with a DELETE to non-existent ID', async () => {
-
+  
+    describe('DELETE /api/v1/palettes/:id', () => {
+      it('Should delete an existing palette in the database', async () => {
+        const paletteToDelete = await database('palettes').first();
+        const paletteId = paletteToDelete.id;
+        await request(app).delete(`/api/v1/palettes/${paletteId}`);
+        const deletedPalette = await database('palettes').where('id', paletteId);
+        expect(deletedPalette.length).toEqual(0);
+      });
+  
+      it('Should return an error with a DELETE to non-existent ID', async () => {
+        const paletteId = 0;
+        const expectedError = `No palette found with the id of ${paletteId}.`;
+        const response = await request(app).delete(`/api/v1/palettes/${paletteId}`);
+        const result = response.body.error;
+        expect(expectedError).toEqual(result);
+      });
     });
   });
 });
