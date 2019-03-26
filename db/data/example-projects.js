298ace015028a2029554b3f53aed79bf5991c20f
shannon <shannonrmoranetz@gmail.com>
2019-03-25 22:54:31 -0600

    Update example hex values

--- db/data/example-projects.js
+++ db/data/example-projects.js
@@ -3,41 +3,41 @@ module.exports = [
         "name": "Warm colors project",
         "palettes": [{
             "name": "Summer colors",
-            "color1": "a12345",
-            "color2": "v34583",
-            "color3": "fff345",
-            "color4": "45slk4",
-            "color5": "k21342"
+            "color1": "FFFF82",
+            "color2": "DDDCF7",
+            "color3": "B5D99C",
+            "color4": "FFAF66",
+            "color5": "E65F5C"
         },
         {
             "name": "Spring colors",
-            "color1": "sd2345",
-            "color2": "83s134",
-            "color3": "f34ss5",
-            "color4": "54slk4",
-            "color5": "21s342"
+            "color1": "F25469",
+            "color2": "BD2D87",
+            "color3": "D664BE",
+            "color4": "DF99F0",
+            "color5": "B191FF"
         }]
     },
     {
         "name": "Cool colors project",
         "palettes": [{
             "name": "Fall colors",
-            "color1": "ssfs45",
-            "color2": "pso134",
-            "color3": "4ssds5",
-            "color4": "s66lk4",
-            "color5": "s34662"
+            "color1": "FCD0A1",
+            "color2": "B1B695",
+            "color3": "A690A4",
+            "color4": "EFA958",
+            "color5": "AFD2E9"
         }]
     },
     {
         "name": "Neutral colors project",
         "palettes": [{
             "name": "Winter colors",
-            "color1": "ffffff",
-            "color2": "a0g9a0",
-            "color3": "o1ijh2",
-            "color4": "r29011",
-            "color5": "12r91j"
+            "color1": "594157",
+            "color2": "726DA8",
+            "color3": "7D8CC4",
+            "color4": "A0D2DB",
+            "color5": "BEE7E8"
         }]
     }
 ]
